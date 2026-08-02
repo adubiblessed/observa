fd7d354d3eea5fce4e20a6b0931d45774a295fc7
adubi <adubiblessed@gmail.com>
2026-08-03 00:51:58 +0100

    project model

--- src/observa/server/model/__init__.py
+++ src/observa/server/model/__init__.py
@@ -1,9 +1,15 @@
 
 from .user import User
 from .accounts import Account
+from .project import Project
+from .team import Team
+from .teammembers import TeamMember
 
 
 __all__ = [
     "User",
     "Account",
+    "Project",
+    "Team",
+    "TeamMember",
 ]
